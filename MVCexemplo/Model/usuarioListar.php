17a78e14520704c840df4a75454925fa9c16df09
Arthur <arthur.providelo@aluno.senai.br>
2026-02-10 13:50:13 -0300

    primeiro MVC

---
+++ MVCexemplo/Model/usuarioListar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Usários</h2>
+    <table border="1">
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+        </tr>
+        <?php foreach($usuarios as $u): ?>
+            <tr>
+                <td><?=$u['nome']?></td>
+                <td><?=$u['email']?></td>
+                <td>próxima aula</td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
---
+++ MVCexemplo/View/UsuarioCadastrar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Cadastro</title>
+</head>
+<body>
+    <form method="POST" action="salvar">
+        <input type="text" name="nome" placeholder="Seu nome" require>
+        <input type="email" name="email" placeholder="Seu email" require>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
