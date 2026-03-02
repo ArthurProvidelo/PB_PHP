7b27c666319eeca699eb4be8aecc450112db9004
Arthur <arthur.providelo@aluno.senai.br>
2026-03-02 07:31:47 -0300

    projetoMVC 02-03

--- MVCexemplo/Model/UsuarioModel.php
+++ MVCexemplo/Model/UsuarioModel.php
@@ -24,4 +24,19 @@ class Usuario{
         //retorna a lista de usuarios
         return $_SESSION['usuarios'] ?? [];
     }
+
+    public static function buscar($id){
+        return $_SESSION['usuarios'][$id] ?? null;
+    }
+
+    public function atualizar($id){
+        if(isset($_SESSION['usuarios'][$id])){ // verifica se o usuário existe
+            $_SESSION['usuarios'][$id] = [ // atualizar com novos dados
+                'nome' => $this->nome,
+                'email' => $this->email
+            ];
+        }
+    }
+
+
 }
