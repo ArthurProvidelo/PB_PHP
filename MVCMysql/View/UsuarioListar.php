83cf7f72d8af9be80f423dd799add1f8503a09fb
Arthur <arthur.providelo@aluno.senai.br>
2026-03-03 13:07:01 -0300

    projeto MVC salvando no Banco de Dados

---
+++ MVCMysql/View/UsuarioListar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabela de usuários 🎲</title>
+</head>
+<body>
+    <a href="/PB_PHP/MVCMysql/usuario/telaCadastro">Ir para tela Cadastar</a>
+    <h2>Usuários</h2>
+    <table border="1">
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>  
+        </tr>
+        <?php foreach($usuarios as $id => $u): ?>
+            <tr>
+                <td><?=$u['Nome']?></td>
+                <td><?=$u['Email']?></td>
+                <td>
+                    <a href="/PB_PHP/MVCMysql/usuario/telaEditar?id=<?= $u['ID'] ?>">Editar✍🏻</a> <a href="/PB_PHP/MVCMysql/usuario/excluir?id=<?= $u['ID'] ?>">Excluir🗑️</a>
+                </td>
+
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
